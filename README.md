5fa9663f0db7072945ef901ad4eb5eb7af7d99d0
guojianli02 <guojianli02@kaikeba.com>
2022-01-11 14:50:05 +0800

    feat: 模板配置

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # xiumu-cli
 脚手架工具
+
+## 安装
+
+`yarn add @xiumu/cli -g`
+
+## 创建项目
+
+`xiumu init <projectName>`
+
+输入项目名字，选择对应的vue或者react模板
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
-// const ora = require('ora');
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { getAll } = require('./lib/gitApi');
+const ora = require('ora')
+const downloadGit  = require('download-git-repo');
 const program = new Command();
 
 class Init {
@@ -14,58 +14,34 @@ class Init {
       .option('-v, --version', '查看当前版本');
 
     program
-      .command('init <templateName> [projectName]')
-      .description('初始化项目模版')
-      .action(() => {
-        this.initCli(...process.argv.slice(3));
+      .command('init <projectName>')
+      .description('create a new project')
+      .action((projectName, options) => {
+        console.log(projectName, options)
+        inquirer
+          .prompt([
+            {
+              type: 'list',
+              name: 'template',
+              message: 'please select frame type',
+              choices: ['Vue', 'React']
+            }
+          ])
+          .then((answer) => {
+            const spinner = ora('downloading template ...');
+            spinner.start();
+            const template = answer.template === 'Vue' ? 'vue-template' : 'react-template';
+            const api = `github:xiumubai/cli-template-store#${template}`;
+            downloadGit(api, projectName, (err) =>{
+              if (err) {
+                spinner.fail('模板下载失败')
+              } else {
+                spinner.succeed('模板下载成功')
+              }
+            })
+          })
       });
     program.parse(process.argv);
-  }
-
-  initCli(templateName, projectName) {
-    console.log(templateName, projectName);
-
-    if (!fs.existsSync(projectName)) {
-      console.log(1);
-      inquirer
-        .prompt([
-          {
-            name: 'description',
-            message: 'Please enter the project description: ',
-          },
-          {
-            name: 'author',
-            message: 'Please enter the author name: ',
-          },
-        ])
-        .then((answer) => {
-          // let spinit = ora('downloading template ...');
-          // spinit.start();
-          console.log(answer.description, answer.author);
-
-          this.downloadLocal(templateName, projectName)
-            .then(() => {})
-            .catch(() => {});
-        });
-    } else {
-      console.log(3);
-    }
-  }
-  downloadLocal(templateName, projectName) {
-    let config = getAll();
-    let api = `${config.registry}/${templateName}`;
-    console.log('api', api);
-    return new Promise((resolve, reject) => {
-      resolve(1);
-    });
-    // return new Promise((resolve, reject) => {
-    //   downloadGit(api, projectName, (err) => {
-    //     if (err) {
-    //       reject(err);
-    //     }
-    //     resolve();
-    //   });
-    // });
   }
 }
 
